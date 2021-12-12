65a9e0af80d9f6f22b8ddd969d7b768b34c61626
Vilen Topchii <32271530+vtopc@users.noreply.github.com>
2021-12-12 17:21:43 +0200

    added content_type TODO

---
+++ interceptors/content_type.go
@@ -0,0 +1,14 @@
+package interceptors
+
+const (
+	ContentTypeHeaderValue = "application/json; charset=utf-8"
+)
+
+const (
+	contentTypeHeaderName = "Content-Type"
+)
+
+// TODO: add ReqContentType interceptor:
+// 	if req.Body != nil && req.Body != http.NoBody {
+// 		req.Header.Set(contentTypeHeaderName, i.ContentType)
+// 	}
